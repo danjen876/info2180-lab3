5ddaf1d939e2d51e369a44934396dcafede1709b
Daniel Jennings <danieljennings5@gmail.com>
2021-10-10 21:10:12 -0500

    Exercise 4 - setting a winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,7 @@ window.onload = function()
 {
     
     //LOADING PAGE AND STYLING BOARD
-    var x;
+    var x, y;
     var temp;
     var twoCases = ["X", "O"];
     var finished = false; //flag
@@ -15,6 +15,18 @@ window.onload = function()
          //adding each square
         squares[x].classList.add("square");
 
+        //What happens when you hover over
+        squares[x].addEventListener("mouseover", function()
+        {
+            this.classList.add("hover");
+        });
+
+        //What happens when your mouse leaves the square
+        squares[x].addEventListener("mouseout", function()
+        {
+            this.classList.remove("hover");
+        });
+
         //What happens when you click
         squares[x].addEventListener("click", function()
         {
@@ -26,18 +38,31 @@ window.onload = function()
             temp = twoCases[0];
             twoCases[0] = twoCases[1];
             twoCases[1] = temp;
-        });
 
-        //What happens when you hover over
-        squares[x].addEventListener("mouseover", function()
-        {
-            this.classList.add("hover");
-        });
+            //Creating the winning combination set
+            var squareSet = [squares[0].textContent + squares[1].textContent + squares[2].textContent, //horizontal wins
+                            squares[3].textContent + squares[4].textContent + squares[5].textContent,
+                            squares[6].textContent + squares[7].textContent + squares[8].textContent,
 
-        //What happens when your mouse leaves the square
-        squares[x].addEventListener("mouseout", function()
-        {
-            this.classList.remove("hover");
+                            squares[0].textContent + squares[3].textContent + squares[6].textContent, //vertical wins
+                            squares[1].textContent + squares[4].textContent + squares[7].textContent,
+                            squares[2].textContent + squares[5].textContent + squares[8].textContent,
+
+                            squares[2].textContent + squares[4].textContent + squares[6].textContent, //diagonal wins
+                            squares[0].textContent + squares[4].textContent + squares[8].textContent];
+
+            //Checking to see if anyone has won yet
+            for (y=0; y<squareSet.length; y++)
+            {
+                if((squareSet[y] == "XXX") || (squareSet[y] == "OOO")) //if we have 3 in a row
+                {
+                    winstatus = document.getElementById("status");
+                    winstatus.classList.add("you-won");
+                    winstatus.textContent = "Great job player " + twoCases[1] + "! You won!"; //send congratulatory message
+                    finished = true; //flag gets changed for finishing the game
+                }
+            }
+
         });
     }
 }
